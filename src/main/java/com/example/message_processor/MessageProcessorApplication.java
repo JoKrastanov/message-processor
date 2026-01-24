e86b149c235f6e574a538778d95c71ecff04e7b0
Joan Krastanov <jokrastanov@gmail.com>
2026-01-24 19:34:42 +0200

    Springboot project setup

---
+++ src/main/java/com/example/message_processor/MessageProcessorApplication.java
@@ -0,0 +1,15 @@
+package com.example.message_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageProcessorApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(MessageProcessorApplication.class, args);
+	}
+
+}
