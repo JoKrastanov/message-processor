7a78cf86d23145f7166ac00bee22ba4e80e9f2fa
Joan Krastanov <jokrastanov@gmail.com>
2026-01-30 01:30:44 +0200

    Update test config to prevent tests from crashing

--- src/test/java/com/example/message_processor/MessageProcessorApplicationTests.java
+++ src/test/java/com/example/message_processor/MessageProcessorApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.message_processor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MessageProcessorApplicationTests {
 
 	@Test
